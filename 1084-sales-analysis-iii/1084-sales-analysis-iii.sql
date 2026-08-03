84bcc5ac935f723eba63964a753afeebcf7ba670
Akshat Navlani <144344423+akshatnavlani@users.noreply.github.com>
2026-08-03 21:04:19 +0530

    Time: 1394 ms (20.54%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT product_id,product_name FROM Product WHERE product_id IN (SELECT product_id FROM Sales WHERE sale_date between '2019-01-01' and '2019-03-31' and product_id not in(SELECT product_id FROM sales where sale_date not between '2019-01-01' and '2019-03-31'))
